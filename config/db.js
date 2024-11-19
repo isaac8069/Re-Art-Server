a5455f008b66b7ed3d06b2ba0117ee8999fed966
isaac8069 <isaac8069@gmail.com>
2024-11-18 19:57:09 -0500

    Update .DS_Store and package files

--- config/db.js
+++ config/db.js
@@ -1,21 +1,18 @@
 'use strict'
 
-// creating a base name for the mongodb
-// REPLACE THE STRING WITH YOUR OWN DATABASE NAME
-const mongooseBaseName = 'exampleApp'
+// Base name for the MongoDB database
+const mongooseBaseName = 'exampleApp';
 
-// create the mongodb uri for development and test
+// MongoDB URIs for development and test environments
 const database = {
 	development: `mongodb://localhost/${mongooseBaseName}-development`,
 	test: `mongodb://localhost/${mongooseBaseName}-test`,
-}
+};
 
-// Identify if development environment is test or development
-// select DB based on whether a test file was executed before `server.js`
-const localDb = process.env.TESTENV ? database.test : database.development
+// Select the appropriate local database (development or test)
+const localDb = process.env.TESTENV ? database.test : database.development;
 
-// Environment variable MONGODB_URI will be available in
-// heroku production evironment otherwise use test or development db
-const currentDb = process.env.MONGODB_URI || localDb
+// Use production URI if available, otherwise fall back to local DB
+const currentDb = process.env.MONGODB_URI || localDb;
 
-module.exports = currentDb
+module.exports = currentDb;
